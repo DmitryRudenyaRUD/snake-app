daf42cec4efdc1529c0db63ed81a2443cfbafc4f
Rud <rud-f@yandex.ru>
2020-03-10 11:55:09 +0300

    Add React.Suspense

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Routes from './Routes';
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import {store} from './store/State';
 
 
-    ReactDOM.render((
-        <BrowserRouter>
-            <Routes store={store} />
-        </BrowserRouter>
-    ), document.getElementById('root'));
+ReactDOM.render((
+    <BrowserRouter>
+        <Routes store={store}/>
+    </BrowserRouter>
+), document.getElementById('root'));
 
 
